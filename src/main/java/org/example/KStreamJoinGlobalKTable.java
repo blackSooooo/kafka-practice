9186db18d36486442ac037e561547cec52b92f33
blackSooooo <blacksooooo@naver.com>
2023-04-29 19:25:22 +0900

    add KTable, KStream, GlobalKTable application

---
+++ src/main/java/org/example/KStreamJoinGlobalKTable.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.GlobalKTable;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.util.Properties;
+
+public class KStreamJoinGlobalKTable {
+    private static String APP_NAME = "stream-global-table-order-join-app";
+    private static String KAFKA_SERVER = "kafka-study:9092";
+    private static String ADDRESS_TABLE = "address_v2";
+    private static String ORDER_STREAM = "order";
+    private static String ORDER_JOIN_STREAM = "order_join";
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, APP_NAME);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        GlobalKTable<String, String> addressTable = builder.globalTable(ADDRESS_TABLE);
+        KStream<String, String> orderStream = builder.stream(ORDER_STREAM);
+
+        orderStream.join(addressTable,
+                (orderKey, orderValue) -> orderKey,
+                (order, address) -> order + " send to " + address)
+                .to(ORDER_JOIN_STREAM);
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+    }
+}
