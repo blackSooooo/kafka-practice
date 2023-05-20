3930779d53830f7b297db66ba58a6792264dc0ab
blackSooooo <blacksooooo@naver.com>
2023-05-20 16:30:01 +0900

    add complete callback

--- producer-application/src/main/java/org/example/domain/ProducerService.kt
+++ producer-application/src/main/java/org/example/domain/ProducerService.kt
@@ -2,6 +2,7 @@ package org.example.domain
 
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,6 +13,19 @@ class ProducerService(
 
     fun send(topic: String, message: String) {
         logger.info("send message > $message")
-        kafkaTemplate.send(topic, message)
+        kafkaTemplate.send(topic, message).whenComplete { result, e ->
+            when (e) {
+                null -> handleSuccess(result)
+                else -> handleFailure(e)
+            }
+        }
+    }
+
+    private fun handleSuccess(result: SendResult<String, String>) {
+        logger.info("[success] $result")
+    }
+
+    private fun handleFailure(e: Throwable) {
+        logger.info("[failure] ${e.message}, $e")
     }
 }
