aaa7697a1e749205379e0143ce1b4b8e1afffb64
blackSooooo <blacksooooo@naver.com>
2023-05-20 17:30:13 +0900

    add hdfs consumer application

--- consumer-application/src/main/kotlin/org/example/ConsumerApplication.kt
+++ consumer-application/src/main/kotlin/org/example/ConsumerApplication.kt
@@ -1,11 +1,51 @@
 package org.example
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.example.consumer.ConsumerWorker
+import org.slf4j.LoggerFactory
+import java.util.Properties
+import java.util.concurrent.Executors
 
-@SpringBootApplication
-class ConsumerApplication
+class ConsumerApplication {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val workers = mutableListOf<ConsumerWorker>()
 
-fun main(args: Array<String>) {
-    runApplication<ConsumerApplication>(*args)
+    fun main(args: Array<String>) {
+        Runtime.getRuntime().addShutdownHook(ShutDownThread())
+
+        val properties = getProperties()
+
+        val executorService = Executors.newCachedThreadPool()
+
+        for (i in 1..CONSUMER_COUNT) {
+            workers.add(ConsumerWorker(properties, TOPIC_NAME, i))
+        }
+        workers.forEach(executorService::execute)
+    }
+
+    inner class ShutDownThread: Thread() {
+        override fun run() {
+            logger.info("shutdown hook!")
+            workers.forEach(ConsumerWorker::stopAndWakeup)
+        }
+    }
+
+    private fun getProperties(): Properties {
+        val configs = Properties()
+
+        configs[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = BOOTSTRAP_SERVERS
+        configs[ConsumerConfig.GROUP_ID_CONFIG] = GROUP_ID
+        configs[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+        configs[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+
+        return configs
+    }
+
+    companion object {
+        const val BOOTSTRAP_SERVERS = "localhost:9092"
+        const val TOPIC_NAME = "select-color"
+        const val GROUP_ID = "color-hdfs-save-consumer-group"
+        const val CONSUMER_COUNT = 3
+    }
 }
---
+++ consumer-application/src/main/kotlin/org/example/consumer/ConsumerWorker.kt
@@ -0,0 +1,111 @@
+package org.example.consumer
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.errors.WakeupException
+import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+class ConsumerWorker(
+    private val props: Properties,
+    private val topic: String,
+    private val threadName: String,
+    private val consumer: KafkaConsumer<String, String> = KafkaConsumer(props)
+): Runnable {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    private val bufferString = ConcurrentHashMap<Int, List<String>>()
+    private val currentFileOffset = ConcurrentHashMap<Int, Long>()
+
+    constructor(props: Properties, topic: String, number: Int): this(
+        props = props,
+        topic = topic,
+        threadName = "consumer-thread-$number"
+    )
+
+    override fun run() {
+        Thread.currentThread().name = threadName
+        consumer.subscribe(listOf(topic))
+        try {
+            while (true) {
+                val records = consumer.poll(Duration.ofSeconds(1))
+
+                for (record in records) {
+                    addHdfsFileBuffer(record)
+                }
+                saveBufferToHdfsFile(consumer.assignment())
+            }
+        } catch (e: WakeupException) {
+            logger.info("Wakeup consumer!!")
+        } catch (e: Exception) {
+            logger.info("${e.message}", e)
+        } finally {
+            consumer.close()
+        }
+    }
+
+    private fun addHdfsFileBuffer(record: ConsumerRecord<String, String>) {
+        val buffer = bufferString.getOrDefault(record.partition(), listOf()) as MutableList
+        buffer.add(record.value())
+        bufferString[record.partition()] = buffer
+
+        if (buffer.size == 1) {
+            currentFileOffset[record.partition()] = record.offset()
+        }
+    }
+
+    private fun saveBufferToHdfsFile(partitions: Set<TopicPartition>) {
+        partitions.forEach{
+            checkFlushCount(it.partition())
+        }
+    }
+
+    private fun checkFlushCount(partition: Int) {
+        if (bufferString[partition] != null) {
+            if (bufferString[partition]!!.size > FLUSH_RECORD_COUNT - 1) {
+                save(partition)
+            }
+        }
+    }
+
+    private fun save(partition: Int) {
+        if (bufferString[partition]!!.isNotEmpty()) {
+            try {
+                val fileName = "/data/color-$partition-${currentFileOffset[partition]}.log"
+                val config = Configuration()
+                config.set("fs.defaultFS", "hdfs://localhost:9000")
+                val hdfsFileSystem = FileSystem.get(config)
+                val fileOutputStream = hdfsFileSystem.create(Path(fileName))
+                fileOutputStream.writeBytes(StringUtils.join(bufferString[partition], "\n"))
+                fileOutputStream.close()
+
+                bufferString[partition] = listOf()
+            } catch (e: Exception) {
+                logger.info("${e.message}", e)
+            }
+        }
+    }
+
+    fun stopAndWakeup() {
+        logger.info("stopAndWakeup!!!")
+        consumer.wakeup()
+        saveRemainBufferToHdfsFile()
+    }
+
+    private fun saveRemainBufferToHdfsFile() {
+        bufferString.forEach{ (partition, _) ->
+            this.save(partition)
+        }
+    }
+
+    companion object {
+        const val FLUSH_RECORD_COUNT = 10;
+    }
+}
