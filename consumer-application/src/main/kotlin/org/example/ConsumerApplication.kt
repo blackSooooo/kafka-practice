fc35dae983f5459031bb35d2fd9178c4e0b4c03c
blackSooooo <blacksooooo@naver.com>
2023-05-21 18:17:23 +0900

    move main function to static

--- consumer-application/src/main/kotlin/org/example/ConsumerApplication.kt
+++ consumer-application/src/main/kotlin/org/example/ConsumerApplication.kt
@@ -8,38 +8,12 @@ import java.util.Properties
 import java.util.concurrent.Executors
 
 class ConsumerApplication {
-    private val logger = LoggerFactory.getLogger(javaClass)
-    private val workers = mutableListOf<ConsumerWorker>()
-
-    fun main(args: Array<String>) {
-        Runtime.getRuntime().addShutdownHook(ShutDownThread())
-
-        val properties = getProperties()
-
-        val executorService = Executors.newCachedThreadPool()
-
-        for (i in 1..CONSUMER_COUNT) {
-            workers.add(ConsumerWorker(properties, TOPIC_NAME, i))
-        }
-        workers.forEach(executorService::execute)
-    }
-
-    inner class ShutDownThread: Thread() {
+    class ShutDownThread: Thread() {
+        private val logger = LoggerFactory.getLogger(javaClass)
         override fun run() {
             logger.info("shutdown hook!")
             workers.forEach(ConsumerWorker::stopAndWakeup)
         }
-    }
-
-    private fun getProperties(): Properties {
-        val configs = Properties()
-
-        configs[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = BOOTSTRAP_SERVERS
-        configs[ConsumerConfig.GROUP_ID_CONFIG] = GROUP_ID
-        configs[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
-        configs[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
-
-        return configs
     }
 
     companion object {
@@ -47,5 +21,31 @@ class ConsumerApplication {
         const val TOPIC_NAME = "select-color"
         const val GROUP_ID = "color-hdfs-save-consumer-group"
         const val CONSUMER_COUNT = 3
+        private val workers = mutableListOf<ConsumerWorker>()
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Runtime.getRuntime().addShutdownHook(ShutDownThread())
+
+            val properties = getProperties()
+
+            val executorService = Executors.newCachedThreadPool()
+
+            for (i in 1..CONSUMER_COUNT) {
+                workers.add(ConsumerWorker(properties, TOPIC_NAME, i))
+            }
+            workers.forEach(executorService::execute)
+        }
+
+        private fun getProperties(): Properties {
+            val configs = Properties()
+
+            configs[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = BOOTSTRAP_SERVERS
+            configs[ConsumerConfig.GROUP_ID_CONFIG] = GROUP_ID
+            configs[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+            configs[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+
+            return configs
+        }
     }
 }
